e232329e3531553137fe285946967fc6ac4429eb
Arthur Alekseev <57834711+arthur100500@users.noreply.github.com>
2022-12-28 03:09:52 +0300

    Fixed save button

--- src/static/editor/js/assemble/assemble.js
+++ src/static/editor/js/assemble/assemble.js
@@ -83,3 +83,22 @@ compileButton.onmousedown = () => {
     fragShader = code;
     draw(glCanvas);
 }
+
+
+const download = (filename, text) => {
+    const element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+saveShButton.onmousedown = () => {
+    code = assamble(Editor.allNodes);
+    download("project.glsl", code);
+}
